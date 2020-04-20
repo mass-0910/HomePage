9e344dd4e0c6730c98812bef5b7946ae510a657c
mass0910 <shakeo.0910@gmail.com>
2020-04-21 00:27:38 +0900

    add block game

--- game/testgame.js
+++ game/testgame.js
@@ -8,19 +8,27 @@ var SCREEN_HEIGHT = 480;
 
 var lastTimeStamp = null;
 
-var x, y;
-var mouse = {x: 0, y: 0};
+var mouse = { x: 0, y: 0 };
+
+var block = [];
+var BLOCK_NUM = (SCREEN_WIDTH / 32) * ( (SCREEN_HEIGHT - 200) / 10);
+
+var player = { x: SCREEN_WIDTH / 2, y: SCREEN_HEIGHT / 2 };
 
 function init(){
+
+    // canvas initialize
     canvas = document.getElementById("maincanvas");
     ctx = canvas.getContext('2d');
     canvas.width = SCREEN_WIDTH;
     canvas.height = SCREEN_HEIGHT;
 
+    // loading assets
     Asset.loadAssets(function(){
         requestAnimationFrame(update);
     });
 
+    // set mousemove listener
     canvas.addEventListener('mousemove', function(evt){
         var canvasRect = canvas.getBoundingClientRect();
         mouse = {
@@ -28,46 +36,58 @@ function init(){
             y: evt.clientY - canvasRect.top
         } 
     });
+
+    // initialize block
+    for(var i = 0; i < BLOCK_NUM; i++){
+        block[i] = { x: (i * 32) % SCREEN_WIDTH, y: Math.floor(i / Math.floor(SCREEN_WIDTH / 32)) * 10};
+        console.log("index:" + i + " x:" + block[i].x + " y:" + block[i].y);
+    }
 };
 
 function update(timestamp){
+
+    // time from before frame
     var delta = 0;
     if(lastTimeStamp != null){
         delta = (timestamp - lastTimeStamp) / 1000;
     }
     lastTimeStamp = timestamp;
-    x = mouse.x;
-    y = mouse.y;
+
     draw();
 
     requestAnimationFrame(update);
 };
 
 function draw(){
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.drawImage(Asset.images['back'], 0, 0);
-    ctx.drawImage(Asset.images['chara'], x, y);
+    block.forEach(function(a_block){
+        ctx.drawImage(Asset.images['block'], a_block.x, a_block.y);
+    });
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(mouse.x - 32, 400, 64, 10);
 };
 
 var Asset = {};
 
 Asset.assets = [
     { type: 'image', name: 'back', src: 'img/black.png' },
-    { type: 'image', name: 'chara', src: 'img/player.png'}
+    { type: 'image', name: 'block', src: 'img/block.png'}
 ];
 
-Asset.images = {};
+Asset.images = [];
 
 Asset.loadAssets = function(onComplete){
+
     var total = Asset.assets.length;
     var loadCount = 0;
 
     var onLoad = function(){
         loadCount++;
-        console.log("count : " + loadCount);
         if(loadCount >= total){
-            console.log("ほな、また。。。");
             onComplete();
         }
     };
