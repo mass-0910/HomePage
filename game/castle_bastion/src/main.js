b910bddc57517148d6e3c3d5b166ed036c030c4d
mass0910 <shakeo.0910@gmail.com>
2020-07-08 11:01:12 +0900

    deploy at 2020/07/08 11:01:12

--- game/castle_bastion/src/main.js
+++ game/castle_bastion/src/main.js
@@ -129,7 +129,7 @@ class Ordering{
             ctx.fillText(String(this.dice.getNum()), this.player_dice_x + 20, this.player_dice_y - 20);
         }
         if(['right alignment', 'result'].indexOf(this.state) != -1){
-            ctx.fillText(String(this.e_dice.getNum()), this.enemy_dice_x + 10, this.enemy_dice_y - 20);
+            ctx.fillText(String(this.e_dice.getNum()), this.enemy_dice_x + 20, this.enemy_dice_y - 20);
         }
     }
 
